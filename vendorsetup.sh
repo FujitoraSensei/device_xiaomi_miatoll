rayavanindra/android_device_xiaomi_miatoll

miatoll: Add vendorsetup.sh

vendorsetup.sh
@@ -1,0 +1,5 @@
+git clone https://github.com/rayavanindra/android_device_xiaomi_sm6250-common -b cherish device/xiaomi/sm6250-common
+git clone https://github.com/rayavanindra/android_vendor_xiaomi_sm6250-common -b 13 vendor/xiaomi/sm6250-common 
+git clone https://github.com/rayavanindra/android_vendor_xiaomi_miatoll -b 13 vendor/xiaomi/miatoll 
+git clone --depth=1 https://github.com/rayavanindra/android_kernel_xiaomi_sm6250 -b main kernel/xiaomi/sm6250
+git clone --depth=1 https://github.com/rayavanindra/prebuilts_clang_host_linux-x86_clang-proton prebuilts/clang/host/linux-x86/clang-proton

FujitoraSensei/device_xiaomi_miatoll

vendorsetup.sh
@@ -1,0 +1,5 @@
+git clone https://github.com/rayavanindra/android_device_xiaomi_sm6250-common -b cr device/xiaomi/sm6250-common
+git clone https://github.com/rayavanindra/android_vendor_xiaomi_sm6250-common -b 13 vendor/xiaomi/sm6250-common 
+git clone https://github.com/rayavanindra/android_vendor_xiaomi_miatoll -b 13 vendor/xiaomi/miatoll 
+git clone --depth=1 https://github.com/rayavanindra/android_kernel_xiaomi_sm6250 -b main kernel/xiaomi/sm6250
+git clone --depth=1 https://github.com/rayavanindra/prebuilts_clang_host_linux-x86_clang-proton prebuilts/clang/host/linux-x86/clang-proton